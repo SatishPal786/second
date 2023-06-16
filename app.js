39b6377ce1b215ff6e299000e3125582b86c5236
SatishPal786 <palsatish646@gmail.com>
2023-06-16 18:56:59 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 import express from "express";
 import path from "path";
+const app = express();
+app.listen(7000, () => {
+  console.log("server started on 7000");
+});
+// SatishPal786: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
